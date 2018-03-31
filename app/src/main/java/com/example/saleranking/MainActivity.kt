ab1b2a982ee8849c8309c1414122d77efbeac910
tichinose1 <toyoh.ichinose@unisys.co.jp>
2018-03-31 21:39:31 +0900

    recyclerViewをMainActivityから参照するとこまでやっておく

--- app/src/main/java/com/example/saleranking/MainActivity.kt
+++ app/src/main/java/com/example/saleranking/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.saleranking
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
