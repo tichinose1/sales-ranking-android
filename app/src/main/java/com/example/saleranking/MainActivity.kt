368f7a569d897810635ff37ca87efb036c801ea1
tichinose1 <toyoh.ichinose@unisys.co.jp>
2018-04-01 00:34:59 +0900

    RSS取得

--- app/src/main/java/com/example/saleranking/MainActivity.kt
+++ app/src/main/java/com/example/saleranking/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.saleranking
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val request = Request.Builder()
+                .url("https://rss.itunes.apple.com/api/v1/jp/ios-apps/top-grossing/all/30/non-explicit.json")
+                .build()
+
+        OkHttpClient()
+                .newCall(request)
+                .enqueue(object : Callback {
+                    override fun onResponse(call: Call?, response: Response?) {
+                        Log.d("test", response?.body()?.string())
+                    }
+
+                    override fun onFailure(call: Call?, e: IOException?) {
+                    }
+                })
 
         val results = arrayOf(
                 Result("モンスターストライク", "https://is3-ssl.mzstatic.com/image/thumb/Purple128/v4/d7/3a/ae/d73aae7d-b2c0-998f-c7a1-a2f60215b880/AppIcon-1x_U007emarketing-85-220-7.png/200x200bb.png"),
